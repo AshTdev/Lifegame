34bbe516eef18e1ce33acbe0e1e556f9cc870c7c
G2ll0u <virgil.gregoire@viacesi.fr>
2024-12-05 12:01:23 +0100

    Ajout CheckNeighbours
    Redéfinition dans le cell

--- cell.h
+++ cell.h
@@ -10,7 +10,7 @@ class Cell{
     Cell(int x, int y);
     bool PrepareChange();
     void ChangeStatement();
-    void CheckNeighbours()=0;
+    bool CheckNeighbours(std::vector<std::vector<Cell*>> &Cells, int longueur, int largeur)=0;
 
 }
 
