ed63e766b6d2ae3a7ad98c8543945e1203f84050
G2ll0u <virgil.gregoire@viacesi.fr>
2024-12-05 12:00:24 +0100

    Ajout CheckNeighbours
    Inclusion de CheckNeighbours pour AliveCell

--- AliveCell.cpp
+++ AliveCell.cpp
@@ -3,6 +3,28 @@
 AliveCell::AliveCell(int x, int y){
     Cell::Cell(x, y);
 }
-AliveCell::CheckNeighbours(){
+AliveCell::CheckNeighbours(std::vector<std::vector<Cell*>> &Cells, int longueur, int largeur){
+    int aliveNeighbours=0;
+    for (int i=-1; i<=1; i++){
+        int nx=x+i;
 
+        for (int j=-1; j<=1; j++){
+            int ny=y+j;
+
+            if ((i!=0 || j!=0) && ((nx>=0 && nx<longueur) && (ny>=0&&ny<largueur))){
+
+                if (dynamic_cast<AliveCell*>(Cells[nx][ny])){
+                    aliveNeighbours++;
+                    
+                }
+            }
+        }
+    }
+    if (aliveNeighbours==2 || aliveNeighbours==3){
+        return false;
+    }
+    else{
+        Change=true;
+        return true;
+    }
 }
