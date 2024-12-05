79cd747e356dcc522b4d00eb513b0e586010ec3a
G2ll0u <virgil.gregoire@viacesi.fr>
2024-12-05 11:59:30 +0100

    Ajout CheckNeighbours
    Inclusion de la fonction pour DeadCell

--- DeadCell.cpp
+++ DeadCell.cpp
@@ -3,6 +3,28 @@
 DeadCell::DeadCell(int x, int y){
     Cell::Cell(x, y);
 }
-DeadCell::CheckNeighbours(){
+DeadCell::CheckNeighbours(std::vector<std::vector<Cell*>> &Cells, int longueur, int largeur){
+    int aliveNeighbours=0;
+    for (int i=-1; i<=1; i++){
+        int nx=x+i;
 
+        for (int j=-1; j<=1; j++){
+            int ny=y+j;
+
+            if ((i!=0 || j!=0) && ((nx>=0 && nx<longueur) && (ny>=0&&ny<largueur))){
+
+                if (dynamic_cast<AliveCell*>(Cells[nx][ny])){
+                    aliveNeighbours++;
+                    
+                }
+            }
+        }
+    }
+    if (aliveNeighbours==3){
+        Change=true;
+        return true;
+    }
+    else{
+        return false;
+    }
 }
