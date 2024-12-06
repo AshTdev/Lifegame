201804a7a789077a2505c1193734ac80691b25de
AshT <yanis.bouzjallikht@viacesi.fr>
2024-12-06 10:28:30 +0100

    Ajout du fichier.cpp (pas encore complet)

---
+++ Fichier.cpp
@@ -0,0 +1,60 @@
+#include "Fichier.h"
+#include "AliveCell.h"
+#include "DeadCell.h"
+
+
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+
+GameOfLife::GameOfLife(const string& filename) {
+    readFile(filename);
+}
+
+void GameOfLife::readFile(const string& filename) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        throw runtime_error("Could not open file");
+    }
+
+    string line;
+    getline(file, line);
+    istringstream iss(line);
+    iss >> height >> width;
+
+    matrix.resize(height, vector<Cell*>(width));
+
+    for (int i = 0; i < height; ++i) {
+        getline(file, line);
+        istringstream iss(line);
+        for (int j = 0; j < width; ++j) {
+            int cellValue;
+            iss >> cellValue;
+            if (cellValue == 1) {
+                matrix[i][j] = new AliveCell(i, j);
+            } else {
+                matrix[i][j] = new DeadCell(i, j);
+            }
+        }
+    }
+
+    file.close();
+}
+
+void GameOfLife::printMatrix() const {
+    for (const auto& row : matrix) {
+        for (const auto& cell : row) {
+            if (dynamic_cast<AliveCell*>(cell)) {
+                cout << "1 ";
+            } else {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+}
