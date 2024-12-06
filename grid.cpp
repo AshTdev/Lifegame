085a85d75644ee883ac531b9900343ca3245b29e
G2ll0u <virgil.gregoire@viacesi.fr>
2024-12-06 11:35:33 +0100

    Constructeur Grid
    Sujet à modification encore

--- grid.cpp
+++ grid.cpp
@@ -1,12 +1,39 @@
 #include "grid.h"
-#include <SFML/Graphics.hpp>
+#include "AliveCell.h"
+#include "DeadCell.h"
+#include <fstream>
+#include <sstream>
 
-Grid::Grid(int Width, int Height){
-    this->Width=Width;
-    this->Height=Height;
-    Cells.resize(Width, std::vector<Cell*>(Height));
 
-}
-void Grid::Initialization(){
+Grid::Grid(string nom_fichier){
 
+    ifstream fichier(nom_fichier);
+    string line;
+    int i=0, j;
+    string word;
+    int number;
+    while (getline(fichier, line)){
+        j=0;
+        istringstream linestream(line);
+        while (linestream >> word){
+                 
+                number=stoi(word);
+                if (j==0){
+                    if (i==0){
+                    Width=number;}
+                    else{
+                    Height=number;}
+                }
+                else{
+                    if(number==1){
+                        Cells[i-1].push_back(new AliveCell(i-1,j));
+                    }
+                    else{
+                        Cells[i-1].push_back(new DeadCell(i-1,j));
+                    }
+                }
+            j++;
+
+        }i++;
+    }
 }
