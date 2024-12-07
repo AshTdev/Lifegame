8c16648b75289f91a8696d81c393b305114a28a5
AshT <yanis.bouzjallikht@viacesi.fr>
2024-12-07 19:57:27 +0100

    Update grid.cpp

--- grid.cpp
+++ grid.cpp
@@ -17,21 +17,16 @@ vector<vector<Cell*>>& Grid::getCells(){
 }
 
 Grid::Grid(string nom_fichier){
-    cout << "Théo41" << endl;
     ifstream fichier(nom_fichier);
-    cout << "Théo41.5" << endl;
     string line;
     int i=0, j;
     string word;
     int number;
-    cout << "Théo42" << endl;
     bool allocation=false;
     while (getline(fichier, line)){
         j=0;
-        cout << "Théo43" << endl;
         istringstream linestream(line);
         while (linestream >> word){
-                cout << "Théo44" << endl; 
                 number=stoi(word);
                 if (i==0){
                     if (j==0){
@@ -47,17 +42,18 @@ Grid::Grid(string nom_fichier){
                     allocation=false;
                 }   
                     if(number==1){
-                        Cells[i-1][j] = new AliveCell(i-1,j);
-                        cout << "Alive cell" << endl;
-                        cout << typeid(*Cells[i-1][j]).name() << endl;
+                        Cells[i-1][j] = new AliveCell(i-1,j, false);
                     }
-                    else{
-                        Cells[i-1][j] = new DeadCell(i-1,j);
-                        cout << "Dead cell" << endl;
-                        cout << typeid(*Cells[i-1][j]).name() << endl;
+                    else if(number==0){
+                        Cells[i-1][j] = new DeadCell(i-1,j, false);
+                    }
+                    else if(number==2){
+                        Cells[i-1][j] = new DeadCell(i-1,j, true);
+                    }
+                    else if(number==3){
+                        Cells[i-1][j] = new AliveCell(i-1,j, true);
                     }
                 }
-              cout << "Théo45" << endl;
             j++;
 
         }i++;
@@ -66,17 +62,18 @@ Grid::Grid(string nom_fichier){
 bool Grid::Iteration(){
     for(int i=0;i<Height;i++){
         for (int j=0;j<Width;j++){
+            if (!(Cells[i][j]->getObstacle())){
                 Cells[i][j]->CheckNeighbours(Cells, Width, Height);
-        }}
+        }}}
     for(int i=0;i<Height;i++){
         for (int j=0;j<Width;j++){
                 if (Cells[i][j]->getChange()==true){
                     if(dynamic_cast<AliveCell*>(Cells[i][j])){ 
                     delete Cells[i][j];
-                    Cells[i][j]=new DeadCell(i,j);
+                    Cells[i][j]=new DeadCell(i,j,false);
                 }else{
                     delete Cells[i][j];
-                    Cells[i][j]=new AliveCell(i,j);
+                    Cells[i][j]=new AliveCell(i,j,false);
                 }
             }
         }    
