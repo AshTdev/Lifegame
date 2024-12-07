c6ec99ae4d5061c5689e823a2c282356bd1b4989
AshT <yanis.bouzjallikht@viacesi.fr>
2024-12-07 19:59:47 +0100

    Ajout obstacle

--- DeadCell.h
+++ DeadCell.h
@@ -2,6 +2,6 @@
 #include <vector>
 
 class DeadCell : public Cell{
-    public:DeadCell(int x, int y);
+    public:DeadCell(int x, int y, bool Obstacle);
     bool CheckNeighbours(std::vector<std::vector<Cell*>> &Cells, int longueur, int largeur);
 };
