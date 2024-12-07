3c00117de65a066aedfe80deb67c564c410f4177
AshT <yanis.bouzjallikht@viacesi.fr>
2024-12-07 20:00:43 +0100

    Bool obstacle (again...)

--- AliveCell.h
+++ AliveCell.h
@@ -1,6 +1,6 @@
 #include "cell.h"
 
 class AliveCell : public Cell{
-    public:AliveCell(int x, int y);
+    public:AliveCell(int x, int y, bool Obstacle);
     bool CheckNeighbours(std::vector<std::vector<Cell*>> &Cells, int longueur, int largeur);
 };
