602e5a8a8a6629152e2cae5412082d43aed1dad5
AshT <yanis.bouzjallikht@viacesi.fr>
2024-12-09 08:59:47 +0100

    Update DeadCell.cpp

--- DeadCell.cpp
+++ DeadCell.cpp
@@ -3,30 +3,34 @@
 #include <iostream>
 using namespace std;
 
+// Constructeur d
 DeadCell::DeadCell(int x, int y, bool Obstacle) : Cell(x, y, Obstacle) {
-
+    // Appelle le constructeur "Cell"
 }
 
+// Méthode pour vérifier les voisins de la cellule
 bool DeadCell::CheckNeighbours(std::vector<std::vector<Cell*>> &Cells, int longueur, int largeur) {
     int aliveNeighbours = 0;
+
+    // On parcout les voisins de la cellule
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
-            if ((i != 0 || j != 0)) {
-                int nx = (x + j + longueur) % longueur;
-                int ny = (y + i + largeur) % largeur;
+            if ((i != 0 || j != 0)) { // Ignore sa propre position
+                int nx = (x + j + longueur) % longueur; // Coordonnée x du voisin
+                int ny = (y + i + largeur) % largeur;  // Coordonnée y du voisin
+
+                // Vérifie si le voisin est une cellule vivant
                 if (dynamic_cast<AliveCell*>(Cells[ny][nx])) {
                     aliveNeighbours++;
                 }
-            else {
-            }
             }
         }
     }
+
+    // Si la cellule morte a exactement 3 voisins vivants --> cellule vivante
     if (aliveNeighbours == 3) {
         return PrepareChange();
-    }
-    else
-     {
+    } else {
         return false;
-    };
+    }
 }
