5c9c3388cbd3e0706d40e58fe2302c9e0f49b77d
AshT <yanis.bouzjallikht@viacesi.fr>
2024-12-09 09:16:26 +0100

    Update cell.h

--- cell.h
+++ cell.h
@@ -2,22 +2,24 @@
 #ifndef CELL
 #define CELL
 
+// Classe abstraite représentant une cellule dans la grille
+class Cell {
+protected:
+    int x; // Coordonnée x de la cellule
+    int y; // Coordonnée y de la cellule
+    bool Change; // Indique un  changement d'état
+    bool Obstacle; // Indique si la cellule est un obstacle
 
-class Cell{
-    protected:
-    int x;
-    int y;
-    bool Change;
-    bool Obstacle;
-    
-    public:
-    Cell(int x, int y, bool Obstacle);
-    bool getObstacle();
-    bool getChange();
-    bool PrepareChange();
-    void ChangeStatement();
-    virtual bool CheckNeighbours(std::vector<std::vector<Cell*>> &Cells, int longueur, int largeur)=0;
+public:
 
+    Cell(int x, int y, bool Obstacle)   // Constructeur 
+    bool getObstacle();  // Return si la cellule est un obstacle
+    bool getChange(); // Return si la cellule est marquée pour changement
+    bool PrepareChange(); 
+    void ChangeStatement(); // Change l'état de la cellule
+
+    // Méthode virtuelle pure qui check les voisins.
+    virtual bool CheckNeighbours(std::vector<std::vector<Cell*>> &Cells, int longueur, int largeur) = 0;
 };
 
 #endif
