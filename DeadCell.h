e35cac80d6c2c360cb4c51abec256f75e913a4da
AshT <yanis.bouzjallikht@viacesi.fr>
2024-12-09 08:58:26 +0100

    Update DeadCell.h

--- DeadCell.h
+++ DeadCell.h
@@ -1,7 +1,12 @@
 #include "cell.h"
 #include <vector>
 
-class DeadCell : public Cell{
-    public:DeadCell(int x, int y, bool Obstacle);
+// Classe représentant une cellule morte, héritage de la classe Cell
+class DeadCell : public Cell {
+public:
+    // Constructeur 
+    DeadCell(int x, int y, bool Obstacle);
+
+    // Méthode pour vérifier les voisins de la cellule
     bool CheckNeighbours(std::vector<std::vector<Cell*>> &Cells, int longueur, int largeur);
 };
