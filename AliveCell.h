67954c883770d63ca172d8147f136ad0b8316b75
AshT <yanis.bouzjallikht@viacesi.fr>
2024-12-09 09:05:32 +0100

    Update AliveCell.h

--- AliveCell.h
+++ AliveCell.h
@@ -1,6 +1,11 @@
 #include "cell.h"
 
-class AliveCell : public Cell{
-    public:AliveCell(int x, int y, bool Obstacle);
+// Classe qui représente la cellule vivante | Héritage de la classe "Cell"
+class AliveCell : public Cell {
+public:
+    // Constructeur 
+    AliveCell(int x, int y, bool Obstacle);
+
+    // Méthode pour vérifier les voisins de la cellule 
     bool CheckNeighbours(std::vector<std::vector<Cell*>> &Cells, int longueur, int largeur);
 };
