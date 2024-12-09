21b90d60c3e6d2c42aa734cde764b40999930571
AshT <yanis.bouzjallikht@viacesi.fr>
2024-12-09 09:20:23 +0100

    Update grid.h

--- grid.h
+++ grid.h
@@ -5,21 +5,24 @@
 
 using namespace std;
 
-
 #ifndef GRID
 #define GRID
 
-class Grid{
-    private:
-    int Width;
-    int Height;
-    std::vector<std::vector<Cell*>> Cells;
-    public:
-    int getWidth();
-    int getHeight();
-    vector<vector<Cell*>>& getCells();
-    Grid(string nom_fichier);
-    bool Iteration();
+// Classe qui représente les cellules de la grille
+class Grid {
+private:
+    int Width; // Largeur de la grille
+    int Height; // Hauteur de la grille
+    std::vector<std::vector<Cell*>> Cells; // Matrice de pointeurs vers les cellules
+
+public:
+    int getWidth();     // Retourne la largeur de la grille
+    int getHeight();    // Retourne la hauteur de la grille
+    vector<vector<Cell*>>& getCells();  // Retourne une référence à la matrice de cellules
+    Grid(string nom_fichier); // Constructeur Grid, permet d'initialiser la grille à partir du fichier fourni par l'utilisateur
+
+
+    bool Iteration(); // Correspond à une itération sur la grille (tout en appliquant les règles du jeu)
 };
 
 #endif
